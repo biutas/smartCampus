bce95ee1debc272d0130af15f5b271b87688bbf8
Gabriel Lopes <biutas@hotmail.com>
2019-12-11 17:07:36 -0300

    db url

--- routes/db.js
+++ routes/db.js
@@ -1,12 +1,16 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'ec2-174-129-255-21.compute-1.amazonaws.com',
-  user     : 'pluixcfucscqgg',
-  password : '4b851aaf1780d5bdb1ca410c8240be0c6076ae1c740538eae0f66321df597221',
-  database : 'dc5hun8fstubfr',
+  host     : 'mysql669.umbler.com',
+  port     : 41890,
+  user     : 'smartcampus',
+  password : 'W|9MM9)vIi',
+  database : 'smartcampus',
   multipleStatements: true
 });
 
-connection.connect();
+connection.connect(err => {
+  if (err) throw err
+  console.log('connected!')
+});
 
 module.exports = connection

9b65402f8732227cf6d38a82a409501a49bed4ef
Gabriel Lopes <biutas@hotmail.com>
2019-12-11 16:50:28 -0300

    change DB address, start command

--- routes/db.js
+++ routes/db.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'smartcampus',
+  host     : 'ec2-174-129-255-21.compute-1.amazonaws.com',
+  user     : 'pluixcfucscqgg',
+  password : '4b851aaf1780d5bdb1ca410c8240be0c6076ae1c740538eae0f66321df597221',
+  database : 'dc5hun8fstubfr',
   multipleStatements: true
 });
 
